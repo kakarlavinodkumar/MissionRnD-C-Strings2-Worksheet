befa0daf5e063abccede9120bcfbb8c8d99bf64e
kakarlavinodkumar <kakarlavinodkumar@missionrnd.com>
2016-01-10 21:20:36 -0800

    Submit Commit

--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -11,6 +11,20 @@ Note:Dont modify original string Neglect Spaces at the right end and at left end
 #include <stdlib.h>
 
 char * get_last_word(char * str){
-
+	int index1,index;
+	char *res;
+	if (str)
+	{
+		for (index1 = index = 0; str[index] != '\0'; index++)
+		{
+			if (str[index] == ' '&&str[index + 1] != ' '&&str[index + 1] != '\0')
+				index1 = index + 1;
+		}
+		res = (char *)malloc(sizeof(char)*(index - index1 + 2));
+		for (index = 0; str[index1] != ' '&&str[index1] != '\0'; index++)
+			res[index] = str[index1++];
+		res[index] = '\0';
+		return res;
+	}
 	return NULL;
 }
